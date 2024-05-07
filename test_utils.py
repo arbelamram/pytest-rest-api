d6933c38538e7d44f01641fddace2540c947948e
Arbel Amram <arbelamram@gmail.com>
2024-05-07 14:58:56 +0300

    remove comments

--- test_utils.py
+++ test_utils.py
@@ -1,11 +1,11 @@
 import uuid
 
 def new_task_payload():
-    user_id = f"test_user_{uuid.uuid4().hex}" # uuid4 is an object, we want to return a string, therefor .hex 
+    user_id = f"test_user_{uuid.uuid4().hex}"
     content = f"test_content_{uuid.uuid4().hex}"
 
     return {
-        "content": content, # "my test content"
-        "user_id": user_id, # "test_user"
+        "content": content,
+        "user_id": user_id,
         "is_done": False,
     }
