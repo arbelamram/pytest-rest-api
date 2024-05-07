1bbc55fbd17ae4203a300900da8bfe32cf227105
Arbel Amram <arbelamram@gmail.com>
2024-05-07 14:58:04 +0300

    error handling

--- tasks.py
+++ tasks.py
@@ -1,21 +1,34 @@
 import requests
 
-ENDPOINT = "https://todo.pixegami.io"
+class ToDoAPI:
+    ENDPOINT = "https://todo.pixegami.io"
 
-def create_task(payload):
-    return requests.put(f"{ENDPOINT}/create-task", json=payload)
+    def __init__(self):
+        self.base_url = self.ENDPOINT
+        
+    def create_task(self, payload):
+        create_task_response = requests.put(f"{self.base_url}/create-task", json=payload)
+        create_task_response.raise_for_status()
+        return create_task_response.json()
 
-def update_task(payload):
-    return requests.put(f"{ENDPOINT}/update-task", json=payload)
+    def get_task(self, task_id):
+        response = requests.get(f"{self.base_url}/get-task/{task_id}")
+        response.raise_for_status()
+        return response.json()
 
-def get_task(task_id):
-    return requests.get(f"{ENDPOINT}/get-task/{task_id}")
+    def get_tasks_list(self, user_id):
+        response = requests.get(f"{self.base_url}/list-tasks/{user_id}")
+        response.raise_for_status()
+        return response.json()
 
-def list_tasks(user_id):
-    return requests.get(f"{ENDPOINT}/list-tasks/{user_id}")
+    def update_task_details(self, payload):
+            response = requests.put(f"{self.base_url}/update-task", json=payload)
+            response.raise_for_status()
+            return response.json()
+    
+    def delete_task(self, task_id):
+        delete_task_response = requests.delete(f"{self.base_url}/delete-task/{task_id}")
+        delete_task_response.raise_for_status()
 
-def delete_task(task_id):
-    return requests.delete(f"{ENDPOINT}/delete-task/{task_id}")
-
-def check_status_code(response, expected_status_code):
-    assert response.status_code == expected_status_code
+        get_task_response = requests.get(f"{self.base_url}/get-task/{task_id}")
+        assert get_task_response.status_code == 404
